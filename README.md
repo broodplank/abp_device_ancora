1be63f3b0c7a945e8c916358803d73b2e7fa537d
broodplank <guichardflorent@gmail.com>
2013-03-30 04:46:56 +0100

    intial commit for gb branch

--- README.md
+++
@@ -1,4 +0,0 @@
-abp_device_ancora
-=================
-
-Android Barebone Packages (ABP) ancora prebuilt folder
---
+++ copy-prebuilt.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ANCORA DEVICE SPEC
+
+BOOTLOADER="ancora"
+
+cp -Rf ${PWD}/prebuilt/* ${PWD}/../../../out/target/${BOOTLOADER}/system
